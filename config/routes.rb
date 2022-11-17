e4c1483deafaa05d8471cb483c8cd11c071981a2
Rose van der Giessen <rosevandergiessen@proton.me>
2022-11-17 16:52:53 +0100

    BookmarksController

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists do
-    resources :bookmarks
-  end
+  root to: 'lists#index'
 
-  resources :movies
+  resources :lists, only: %i[index show new create destroy] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: [:destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
